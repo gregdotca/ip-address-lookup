b92d559fbbb9284b32254d339024da9ecc1ec358
Greg Chetcuti <greg@chetcuti.com>
2025-03-10 18:59:05 -0700

    Refactor

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 from time import sleep
 
 app = Flask(__name__, static_folder="assets")
@@ -8,12 +8,22 @@ app = Flask(__name__, static_folder="assets")
 APP_TITLE = "IP Address Lookup"
 
 
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        ip_address = get_user_ip_address()
+    else:
+        ip_address = str(request.form["ip_address"])
+
+    return redirect("/" + ip_address)
+
+
 @app.route("/<ip_address>", methods=["GET"])
 def home_ip(ip_address):
     sleep(0.25)
     page_body = ""
 
-    if ip_address == get_users_ip_address():
+    if ip_address == get_user_ip_address():
         page_body += "<strong>Your Public IP Address</strong><BR>"
 
     page_body += process_ip_address(ip_address)
@@ -21,38 +31,7 @@ def home_ip(ip_address):
     return display_homepage(ip_address, page_body)
 
 
-@app.route("/", methods=["GET"])
-def home():
-    sleep(0.25)
-
-    ip_address = get_users_ip_address()
-
-    page_body = "<strong>Your Public IP Address</strong><BR>"
-    page_body += process_ip_address(ip_address)
-
-    return display_homepage(ip_address, page_body)
-
-
-@app.route("/", methods=["POST"])
-def home_post():
-    page_body = ""
-
-    sleep(0.25)
-
-    submitted_ip_address = str(request.form["ip_address"])
-
-    if submitted_ip_address == "":
-        submitted_ip_address = get_users_ip_address()
-
-    if submitted_ip_address == get_users_ip_address():
-        page_body += "<strong>Your Public IP Address</strong><BR>"
-
-    page_body += process_ip_address(submitted_ip_address)
-
-    return display_homepage(submitted_ip_address, page_body)
-
-
-def get_users_ip_address():
+def get_user_ip_address():
     return request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
 
 
@@ -68,34 +47,30 @@ def process_ip_address(ip_address):
         page_body += "Org: " + result["org"] + "<BR>"
         page_body += "AS: " + result["as"] + "<BR>"
         page_body += (
-            "Location: <strong>"
-            + result["city"]
-            + ", "
-            + result["regionName"]
-            + " ("
-            + result["region"]
-            + ")"
-            + ", "
-            + result["country"]
-            + " ("
-            + result["countryCode"]
-            + ")"
-            + "</strong>"
-            + "<BR>"
+                "Location: <strong>"
+                + result["city"]
+                + ", "
+                + result["regionName"]
+                + " ("
+                + result["region"]
+                + ")"
+                + ", "
+                + result["country"]
+                + " ("
+                + result["countryCode"]
+                + ")"
+                + "</strong>"
+                + "<BR>"
         )
         page_body += "Postal Code: " + result["zip"] + "<BR>"
         page_body += "Time Zone: " + result["timezone"] + "<BR>"
         page_body += (
-            "Latitude & Longitude: "
-            + str(result["lat"])
-            + ", "
-            + str(result["lon"])
-            + "<BR>"
+                "Latitude & Longitude: "
+                + str(result["lat"])
+                + ", "
+                + str(result["lon"])
+                + "<BR>"
         )
-
-        # result = result.replace("success\n", "")
-        # result = result.replace("\n", "<BR>")
-        # page_body += result
 
     except Exception:
         page_body += "Unable to lookup IP address, please try again."
